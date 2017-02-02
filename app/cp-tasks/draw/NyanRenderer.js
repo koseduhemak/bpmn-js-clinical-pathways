744fecec830b8074176eb6858e2ed35606671d5d
Maximilian Füsslin <mfuesslin@web.de>
2017-02-02 23:08:44 +0100

    first commit: integrated custom service task into custom modeler

---
+++ app/cp-tasks/draw/NyanRenderer.js
@@ -0,0 +1,41 @@
+
+var inherits = require('inherits');
+
+var BaseRenderer = require('diagram-js/lib/draw/BaseRenderer');
+
+var is = require('bpmn-js/lib/util/ModelUtil').is;
+
+var Cat = require('../cat/index');
+
+var svgAppend = require('tiny-svg/lib/append'),
+    svgCreate = require('tiny-svg/lib/create');
+
+
+function NyanRender(eventBus) {
+  BaseRenderer.call(this, eventBus, 1500);
+
+  this.canRender = function(element) {
+    return is(element, 'bpmn:ServiceTask');
+  };
+
+
+  this.drawShape = function(parent, shape) {
+    var url = Cat.dataURL;
+
+    var catGfx = svgCreate('image', {
+      x: 0,
+      y: 0,
+      width: shape.width,
+      height: shape.height,
+      href: url
+    });
+
+    svgAppend(parent, catGfx);
+
+    return catGfx;
+  };
+}
+
+inherits(NyanRender, BaseRenderer);
+
+module.exports = NyanRender;
